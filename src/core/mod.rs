2f4de2aa60a3bbd982079aa8c1c9ce85339b32ae
sanket1729 <sanket1729@gmail.com>
2020-09-10 19:02:56 -0500

    Support for creating Value from sig/pk

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -18,36 +18,8 @@ pub mod term;
 pub mod types;
 
 use crate::bitcoin_hashes::HashEngine;
+use crate::util::{bitvec_to_bytevec, u64_to_array_be};
 use bitcoin_hashes::sha256;
-// handy function for converting bit vector to vec[u8]
-// # PANIC:
-// panics when bitvec length is not a multiple of 8.
-#[allow(dead_code)]
-pub(crate) fn bitvec_to_bytevec(bitvec: &[bool]) -> Vec<u8> {
-    let mut ret = vec![];
-    assert!(bitvec.len() % 8 == 0, "Bitvec len must be multiple of 8");
-    let mut start = 0;
-    while start < bitvec.len() {
-        //read a byte
-        let mut byte: u8 = 0;
-        for i in 0..8 {
-            byte += (bitvec[start + i] as u8) * (1u8 << (7 - i));
-        }
-        ret.push(byte);
-        start += 8;
-    }
-    ret
-}
-
-// handy utlity for u64 to be. requried for converting len
-// in sha2 specification.
-fn u64_to_array_be(val: u64) -> [u8; 8] {
-    let mut res = [0; 8];
-    for (i, byte) in res.iter_mut().enumerate() {
-        *byte = ((val >> ((8 - i - 1) * 8)) & 0xff) as u8;
-    }
-    res
-}
 
 use crate::Value;
 
